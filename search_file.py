6570a2e4d49f944ca8178ebe071a616203caec4d
kaleabmarkos <kaleabgessese@gmail.com>
2024-09-05 20:18:57 -0500

    first commit

---
+++ search_file.py
@@ -0,0 +1,10 @@
+#Define a Node class
+
+#Define a BinarySearchTree class
+    # a function to insert
+    # a function to search
+    # a function to traverse (in order)
+
+#File parsing and Error Handling
+
+#Main Function
