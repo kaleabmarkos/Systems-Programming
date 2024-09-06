ed62962703acb489910c18b845b454821ea6caba
kaleabmarkos <kaleabgessese@gmail.com>
2024-09-05 20:43:29 -0500

    Node Class

--- search_file.py
+++ search_file.py
@@ -8,3 +8,17 @@
 #File parsing and Error Handling
 
 #Main Function
+
+class Node:
+    def __init__(self, symbol, value, rflag):
+        self.symbol = symbol[:4].upper()
+        self.value = value
+        self.rflag = rflag
+        self.iflag = True
+        self.mflag = False
+        self.left = None
+        self.right = None
+    
+    def __repr__(self) -> str:
+        return f"{self.symbol}: {self.value} {self.rflag} {self.iflag} {self.mflag}"
+    
